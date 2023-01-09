0b5575e845ec776672c411f0e7172e0793f88019
cem-1 <106272421+cem-1@users.noreply.github.com>
2023-01-09 22:39:41 +0300

    new updates

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,43 @@
+const Clarifai = require('clarifai');
+
+
+const handleApiCall = (req,res) => {
+    var IMAGE_URL = req.body.input;
+    const USER_ID = 'e6uzz58qbgss';
+    const PAT = 'c9f2fef0e7734711aaef9249726e6d01';
+    const APP_ID = '36611efaf2a540e2b94b1a410d2bf24d';
+    const MODEL_ID = 'face-detection';  
+
+    const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions).then(response =>response.text()).then(response => JSON.parse(response)).then(data => res.json(data))
+    .catch(err => res.status(400).json("unable to work with api"));
+}
+
+
 const handleImage = (req,res,db)=>{
     const {id} = req.body;
     db("users").where("id","=",id).increment("entries",1).returning("entries").then(entries => {
@@ -7,4 +47,5 @@ const handleImage = (req,res,db)=>{
 
 module.exports = {
     handleImage: handleImage,
+    handleApiCall: handleApiCall,
 }
--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req,res,db,bcrypt)=>{
     const{email,name,password} = req.body;
+    if(!email || !name || !password){
+        return res.status(400).json("Incorrect Credentials!");
+    } 
     const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
         trx.insert({
@@ -20,6 +23,7 @@ const handleRegister = (req,res,db,bcrypt)=>{
     })
     .catch(err => res.status(400).json("unable ro register"));;
 }
+
 
 module.exports = {
     handleRegister: handleRegister,
